d7163380978b83406699cf6b43bc8a916b5d4f97
Alexander Ananchenko <snvtr@mail.ru>
2023-05-21 14:36:05 +0200

    do_all.sh #2

--- files/do_all.sh
+++ files/do_all.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+set -e
+
 # update/install git, pip3
-apt update && apt install -y git python3-pip 
+sudo apt update && apt install -y git python3-pip 
+
+USER_NAME=$(cat files/user.env)
 
 # create user
-useradd -m -s /bin/bash $USER_NAME
+sudo useradd -m -s /bin/bash $USER_NAME
+sudo mkdir ~${USER_NAME}/.ssh/ && cp ~ubuntu/.ssh/* ${USER_NAME}/.ssh/
+sudo chown ${USER_NAME}.${USER_NAME} ~${USER_NAME}/.ssh/*
 
-cd "~$USER_NAME"
+cd "~${USER_NAME}" || exit 1
 
 # clone and install antispam-bot:
 git clone git@github.com:snvtr/antispam-bot.git
-cd antispam-bot || exit
+cd ~${USER_NAME}/antispam-bot || exit 1
 sed -i "s|{{ UserName }}|$USER_NAME|g" antispam-bot.service
 sudo cp antispam-bot.service /etc/systemd/system/
 sudo pip3 -install requirements.txt
@@ -21,6 +27,7 @@ sudo cp challenge.py /usr/local/lib/${PYTHON_VER}/telegram_antispam_bot/
 
 # clone and install kaztili-bot:
 git clone git@github.com:snvtr/KaztiliSozdikBot.git
+cd ~${USER_NAME}/KaztiliSozdikBot || exit 1
 sed -i "s|{{ UserName }}|$USER_NAME|g" kaztili-bot.service
 sudo cp kaztili-bot.service /etc/systemd/system/
 pip3 -install requirements.txt
@@ -28,11 +35,14 @@ sudo chmod 0755 aiogram_bot.py
 
 # clone and install runcalc-bot:
 git clone git@github.com:snvtr/runcalc-bot.git
+cd ~${USER_NAME}/runcalc-bot || exit 1
 sed -i "s|{{ UserName }}|$USER_NAME|g" runcalc-bot.service
 sudo cp runcalc-bot.service /etc/systemd/system/
 pip3 -install requirements.txt
 sudo chmod 0755 aiogram_bot.py
 
 sudo systemctl daemon-reload
+
+#sudo userdel -r ubuntu
 
 #
